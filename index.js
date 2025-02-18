93e41931cd46b77aa1391f5bdcc37083e26884fb
g8t0rboi <aaron.estrellado@gmail.com>
2025-02-18 14:35:27 -0500

    Add functional UI to game

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 let computerScore = 0;
 let humanScore = 0;
 
-const scoreElement = document.getElementById('score');
+
+const results = document.getElementById('result');
+const rockButton = document.getElementById('rock');
+const paperButton = document.getElementById('paper');
+const scissorsButton = document.getElementById('scissors');
+const humanScoreElement = document.getElementById('player-score');
+const computerScoreElement = document.getElementById('computer-score');
 
 const getComputerChoice = () => {
     const choices = ["rock", "paper", "scissors"];
@@ -34,27 +40,36 @@ const playRound = (humanChoice, computerChoice) => {
     }
 }
 
-const playGame = () => {
+rockButton.addEventListener('click', () => {
+    results.textContent = playRound("rock", getComputerChoice());
+    humanScoreElement.textContent = humanScore;
+    computerScoreElement.textContent = computerScore;
 
-    for (let i = 0; i < 5; i++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        const result = playRound(humanSelection, computerSelection);
-        console.log(result);
-    }
+    checkScore();
+});
 
-    if (humanScore === computerScore) {
-        return "You tied the game";
-    } 
-    else if (humanScore > computerScore) {
-        return "You won the game";
-    }
-    else {
-        return "You lost the game";
-    }
+paperButton.addEventListener('click', () => {
+    results.textContent = playRound("paper", getComputerChoice());
+    humanScoreElement.textContent = humanScore;
+    computerScoreElement.textContent = computerScore;
 
+    checkScore();
+});
+
+scissorsButton.addEventListener('click', () => {
+    results.textContent = playRound("scissors", getComputerChoice());
+    humanScoreElement.textContent = humanScore;
+    computerScoreElement.textContent = computerScore;
+
+    checkScore();
+});
+
+const checkScore = () => {
+    if (humanScore === 5 || computerScore === 5) {
+        if (humanScore === 5) {
+            alert("You won the game");
+        } else {
+            alert("You lost the game");
+        }
+    }
 }
-
-const game = playGame();
-console.log(game);
-scoreElement.innerHTML = game;
